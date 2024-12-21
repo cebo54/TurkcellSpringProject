9dab7085e06b2869ed9ec7796bf078d777466a10
cebo54 <kayacebrail1905@gmail.com>
2024-12-21 12:27:08 +0300

    Initial Commit

---
+++ src/main/java/com/turkcell/turkcellspringboot/TurkcellSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.turkcell.turkcellspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurkcellSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TurkcellSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turkcell/turkcellspringboot/TurkcellSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.turkcellspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurkcellSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
